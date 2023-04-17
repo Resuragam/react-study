68c8c3caad5bb1f6591ac94582d1fc69d4b8b159
Resuragam <1791614500@qq.com>
2023-04-17 15:24:25 +0800

    feat: 编写第一个React应用

--- src/index.js
+++ src/index.js
@@ -5,3 +5,23 @@
 */
 
 import React from 'react'
+// ReactDom 渲染组件模块
+import ReactDom from 'react-dom'
+import { createRoot } from "react-dom/client"
+
+// React18 不再支持ReactDom.render方式进行渲染
+// ReactDom.render(<div>22222</div>, document.getElementById('root'))
+
+// React18 渲染方式
+const root = createRoot(document.getElementById('root'))
+root.render(<div>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+    </ul>
+</div>)
+
+/*
+* jsx = js + xml
+* */
